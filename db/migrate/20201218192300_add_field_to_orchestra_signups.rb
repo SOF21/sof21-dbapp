0134256083f33102c3f115170acfb02841f30b49
Michelle K <michkrej@gmail.com>
2021-01-13 22:49:09 +0100

    Update 20201218192300_add_field_to_orchestra_signups.rb

--- db/migrate/20201218192300_add_field_to_orchestra_signups.rb
+++ db/migrate/20201218192300_add_field_to_orchestra_signups.rb
@@ -1,4 +1,4 @@
-class AddFieldToOrchsetraSignups < ActiveRecord::Migration[5.0]
+class AddFieldToOrchestraSignups < ActiveRecord::Migration[5.0]
   def change
     add_column :orchestra_signups, :pickup_with, :boolean
   end
