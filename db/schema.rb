a2849ec3fa39254d34b7019d1b67a3da3aebadd9
Michelle <mickr592@student.liu.se>
2021-01-19 20:48:21 +0100

    Added flags to cortege

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210118174409) do
+ActiveRecord::Schema.define(version: 20210119194103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,7 @@ ActiveRecord::Schema.define(version: 20210118174409) do
     t.boolean  "electricity",                  default: false,     null: false
     t.string   "other_comments",               default: "",        null: false
     t.string   "contact_mail"
+    t.text     "flags",                        default: [],                     array: true
     t.index ["user_id"], name: "index_corteges_on_user_id", using: :btree
   end
 
