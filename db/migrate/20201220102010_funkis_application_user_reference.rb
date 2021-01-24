5c9bfc9c631865e0b1f69017a2d962288a86b7b2
Michelle K <michkrej@gmail.com>
2021-01-24 22:11:18 +0100

    Delete 20201220102010_funkis_application_user_reference.rb

--- db/migrate/20201220102010_funkis_application_user_reference.rb
+++
@@ -1,7 +0,0 @@
-class FunkisApplicationUserReference < ActiveRecord::Migration[5.0]
-  def change
-    change_table :funkis_applications do |t|
-      t.belongs_to :user, index: true
-    end
-  end
-end
