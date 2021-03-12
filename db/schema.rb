2df039cbc10a8e816aa52b321bd46a296584e0b0
Dag Jönsson <dag@jnsson.eu>
2021-03-12 15:36:10 +0100

    merge develop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210215175555) do
+ActiveRecord::Schema.define(version: 20210222184717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -179,8 +179,10 @@ ActiveRecord::Schema.define(version: 20210215175555) do
     t.boolean  "marked_done",           default: false
     t.boolean  "booking_sent",          default: false
     t.boolean  "checked_in",            default: false
+    t.integer  "user_id"
     t.index ["funkis_application_id"], name: "index_funkis_on_funkis_application_id", using: :btree
     t.index ["funkis_category_id"], name: "index_funkis_on_funkis_category_id", using: :btree
+    t.index ["user_id"], name: "index_funkis_on_user_id", using: :btree
   end
 
   create_table "funkis_applications", force: :cascade do |t|
@@ -424,4 +426,5 @@ ActiveRecord::Schema.define(version: 20210215175555) do
   add_foreign_key "discount_codes", "products"
   add_foreign_key "funkis", "funkis_applications"
   add_foreign_key "funkis", "funkis_categories"
+  add_foreign_key "funkis", "users"
 end
