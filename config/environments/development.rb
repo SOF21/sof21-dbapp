1edef475a5884d8d5a9e8b72ad9116202a732304
Michelle <mickr592@student.liu.se>
2021-03-16 21:43:56 +0100

    Changed mailcatcher address to ip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,8 @@ Rails.application.configure do
 
   # Mailcatcher
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
